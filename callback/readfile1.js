163c2a5762bf00d4fc2adb15f6b41eedd3496d94
Alice <a0975012712@gmail.com>
2022-01-09 05:13:21 +0800

    readfile1.js promise

--- callback/readfile1.js
+++ callback/readfile1.js
@@ -1,10 +1,20 @@
 const { readFile } = require("fs");
 
-readFile("test.txt", "utf-8", (err, data) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  console.log(data);
-  // insert to mysql
+let readFilePromise = new Promise((resolve, reject) => {
+  // 當非同步作業成功時，呼叫 resolve(...),而失敗時則呼叫 reject(...)。
+
+  readFile("test.txt", "utf-8", (err, data) => {
+    if (err) {
+      reject(err);
+    }
+    resolve(data);
+    // insert to mysql
+  });
 });
+
+readFilePromise.then((data) => {
+  console.log(data);
+})
+  .catch((err) => {
+    console.log(err);
+  });
