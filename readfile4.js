d5cea0840ed64ec6ebff46b5e1763fc2db2de614
alice <a0975012712@gmail.com>
2022-01-09 17:19:16 +0800

    fix readfile4

--- readfile4.js
+++ readfile4.js
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-// Promise 物件代表一個即將完成、或失敗的非同步操作，以及它所產生的值。
-
-const { readFile } = require("fs/promises");
-
-(async() => {
-
-  try {
-    let result1 = await readFile("test.txt", "utf-8");
-    console.log(`${result1}`);  
-    
-  } catch (err) {
-    console.error(err);
-  }
-})();
-
-//..
-=======
 // Promise 物件代表一個即將完成、或失敗的非同步操作，以及它所產生的值。
 
 const { readFile } = require("fs/promises");
@@ -31,4 +13,3 @@ const { readFile } = require("fs/promises");
   }
 
 })();
->>>>>>> b5aac3b66738fd785388d8a78eff2c544a095a5b
